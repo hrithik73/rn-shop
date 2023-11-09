1d7322f4596d2ba5d2d9ed0f1cc6b0ae219c9b45
Hritik Singh <hrithiksinghvps_cse18@its.edu.in>
2023-11-09 15:04:18 +0530

    chore: Project init with expo

--- App.tsx
+++ App.tsx
@@ -1,54 +1,20 @@
-import { StripeProvider } from '@stripe/stripe-react-native';
-import React, { useEffect, useState } from 'react';
-import { InstantSearch } from 'react-instantsearch-hooks';
-import { StatusBar } from 'react-native';
-import AntDesign from 'react-native-vector-icons/AntDesign';
-import Feather from 'react-native-vector-icons/Feather';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-import { ALGOLIA_INDEX_NAME, searchClient } from '@src/configs/algolia';
-import useApi from '@src/hooks/useApi';
-import usePushNotification from '@src/hooks/usePushNotification';
-import AppNavigator from '@src/navigators';
-import { persistor, store } from '@src/redux/store';
-
-const App = () => {
-  const [publishableKey, sestPublishableKey] = useState('');
-  const { getPublishableKey } = useApi();
-
-  useEffect(() => {
-    // Loading fonts for Vector icons in IOS
-    // TODO:- Need a Permanent fix
-    Feather.loadFont();
-    AntDesign.loadFont();
-
-    const getKey = async () => {
-      const key = await getPublishableKey();
-      sestPublishableKey(key);
-    };
-
-    getKey();
-  }, [getPublishableKey]);
-
-  usePushNotification();
-
+export default function App() {
   return (
-    <Provider store={store}>
-      <StatusBar barStyle="dark-content" />
-      <PersistGate loading={null} persistor={persistor}>
-        <StripeProvider
-          publishableKey={publishableKey}
-          merchantIdentifier="merchant.identifier">
-          <InstantSearch
-            searchClient={searchClient}
-            indexName={ALGOLIA_INDEX_NAME}>
-            <AppNavigator />
-          </InstantSearch>
-        </StripeProvider>
-      </PersistGate>
-    </Provider>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
-};
+}
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,16 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        alias: {
-          '@src': './src',
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '~': './src',
+          },
         },
-      },
+      ],
     ],
-  ],
+  };
 };
